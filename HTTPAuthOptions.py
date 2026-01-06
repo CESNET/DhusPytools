d282fc14243bf72f43b900a6ca2a1e58f6b973b4
Jan Krcmar <honza801@gmail.com>
2026-01-06 09:49:01 +0100

    KeycloakTokenAuth do not generate token_url if realm not defined
    Assuming the url contains full path including the realm
    
    +class HTTPAuthRegistry

--- HTTPAuthOptions.py
+++ HTTPAuthOptions.py
@@ -5,6 +5,7 @@ import logging
 import netrc
 from urllib.parse import urlparse
 
+
 # Custom authentication class for Bearer Token
 class HTTPBearerAuth(AuthBase):
     def __init__(self, token_file=".token"):
@@ -55,16 +56,20 @@ class FileBasedBasicAuth(HTTPBasicAuth):
 
 class KeycloakTokenAuth(HTTPBearerAuth):
     def __init__(self, server_url, realm, client_id, client_secret=None, netrc_file=None, username=None, password=None):
-        logging.debug(f"Initializing {type(self)}")
         self.server_url = server_url
         self.realm = realm
         self.client_id = client_id
         self.client_secret = client_secret
         self.netrc_file = netrc_file
-        self.token_url = f"{server_url}/realms/{realm}/protocol/openid-connect/token"
+        if realm:
+            self.token_url = f"{server_url}/realms/{realm}/protocol/openid-connect/token"
+        else:
+            logging.debug(f"HTTP KeycloakTokenAuth realm not defined, using server url as is: {server_url}")
+            self.token_url = server_url
         if not (username and password):
             username, password = self._read_credentials()
         self.token = self._get_token(username, password)
+        logging.debug(f"Initialized {type(self)} for {self.token_url}")
 
     def _read_credentials(self):
         n = netrc.netrc(self.netrc_file)
@@ -92,3 +97,48 @@ class KeycloakTokenAuth(HTTPBearerAuth):
             return access_token
         else:
             raise Exception(f"Failed to get token: {response.status_code}, {response.text}")
+
+    def __str__(self):
+        return self.token_url
+
+
+class HTTPAuthRegistry:
+    def __init__(self):
+        self._http_auth = []
+
+    def add(self, auth_config):
+        http_auth = self._create_auth(auth_config)
+        self._http_auth.append(http_auth)
+        return http_auth
+
+    def get_by_token_endpoint(self, auth_config):
+        if not 'tokenEndpoint' in auth_config:
+            raise ValueError("auth_token.tokenEndpoint must be provided to get Auth")
+
+        token_endpoint = auth_config['tokenEndpoint']
+        for http_auth in self._http_auth:
+            if http_auth.token_url == token_endpoint:
+                logging.debug(f'Using existing HTTPAuth instance {http_auth}')
+                return http_auth
+        return self.add(auth_config)
+
+    def _create_auth(self, auth_config, netrc_file = None):
+        realm = auth_config.get('realm')
+        if 'user' in auth_config and 'password' in auth_config:
+            auth = KeycloakTokenAuth(
+                server_url=auth_config['tokenEndpoint'],
+                realm=realm,
+                client_id=auth_config['clientId'],
+                username=auth_config['user'],
+                password=auth_config['password'],
+            )
+        else:
+            auth = KeycloakTokenAuth(
+                server_url=auth_config['tokenEndpoint'],
+                realm=realm,
+                client_id=auth_config['clientId'],
+                netrc_file=netrc_file,
+            )
+
+        return auth
+
