102d21b0b57248fc8d01781424fdf24ad784a8d0
Jan Krcmar <honza801@gmail.com>
2026-06-09 12:45:10 +0200

    push metrics to vm +collgs

--- push_metrics_to_vm.py
+++ push_metrics_to_vm.py
@@ -27,7 +27,7 @@ def get_endpoint_ip(namespace, service):
     port = ports[0]["port"]
     return f"{ip}:{port}"
 
-def parse_kafka(output: str):
+def parse_kafka(output: str, namespace: str):
     metrics = []
     for line in output.splitlines():
         # skip headers and empty lines
@@ -49,7 +49,7 @@ def parse_kafka(output: str):
         client_id = parts[6]
 
         #labels = f'group="{group}",topic="{topic}",partition="{partition}",client="{client_id}"'
-        labels = f'group="{group}",topic="{topic}",partition="{partition}"'
+        labels = f'namespace="{namespace}",group="{group}",topic="{topic}",partition="{partition}"'
 
         metrics.append(f'kafka_consumer_offset{{{labels}}} {current_offset}')
         metrics.append(f'kafka_logend_offset{{{labels}}} {logend_offset}')
@@ -66,15 +66,19 @@ if __name__ == "__main__":
     # http://vms-victoria-metrics-single-server.monitoring.svc.cluster.local.:8428
     VM_HOST = get_endpoint_ip('monitoring', 'vms-victoria-metrics-single-server')
     VICTORIA_URL = f"http://{VM_HOST}/api/v1/import/prometheus"
-    COMMAND = ["/root/bin/kafka-get-queue-informations.sh", "relay", "-q"]
+    COMMANDS = [
+        ("relay", ["/root/bin/kafka-get-queue-informations.sh", "relay", "-q"]),
+        ("default", ["/root/bin/kafka-get-queue-informations.sh", "default", "-q"]),
+    ]
 
     GSS_URL = "https://gss.vm.cesnet.cz/odata/v1"
 
     metrics = []
 
-    # Run the command and capture output
-    proc = subprocess.run(COMMAND, capture_output=True, text=True, check=True)
-    metrics += parse_kafka(proc.stdout)
+    # Run the commands and capture output
+    for namespace, command in COMMANDS:
+        proc = subprocess.run(command, capture_output=True, text=True, check=True)
+        metrics += parse_kafka(proc.stdout, namespace)
     metrics += check_product_counts.gss_metrics(GSS_URL)
     
     metrics_text = "\n".join(metrics) + "\n"
@@ -86,4 +90,3 @@ if __name__ == "__main__":
     resp = requests.post(VICTORIA_URL, data=metrics_text.encode("utf-8"))
     resp.raise_for_status()
     logging.debug(f"Pushed metrics successfully: {resp.text}")
-
